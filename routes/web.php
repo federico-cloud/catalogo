b330e67d1e60e1b9fa793daffb89721423138570
Federico Galan <fegv.2@hotmail.com.ar>
2021-06-29 15:23:37 -0300

    Se agrega la funcion validarForm en el ProductoController

--- routes/web.php
+++ routes/web.php
@@ -60,3 +60,5 @@ use App\Http\Controllers\ProductoController;
 Route::get('/adminProductos', [ProductoController::class, 'index']);
 
 Route::get('/agregarProducto', [ProductoController::class, 'create']);
+
+Route::post('/agregarProducto', [ProductoController::class, 'store']);
